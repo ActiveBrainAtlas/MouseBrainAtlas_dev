2f0e924d92f785205c3d3af46898283d4fe78c5d
KuiQian <qkprince92@gmail.com>
2019-05-30 14:06:41 -0700

    generate regions

--- src/utilities/metadata.py
+++ src/utilities/metadata.py
@@ -508,7 +508,8 @@ classifier_settings = read_csv(CLASSIFIER_SETTINGS_CSV, header=0, index_col=0)
 registration_settings = read_csv(REGISTRATION_SETTINGS_CSV, header=0, index_col=0)
 preprocess_settings = read_csv(PREPROCESS_SETTINGS_CSV, header=0, index_col=0)
 detector_settings = read_csv(DETECTOR_SETTINGS_CSV, header=0, index_col=0)
-windowing_settings = {1: {"patch_size": 224, "spacing": 56},
+windowing_settings = {0:{"patch_size": 34, "spacing": 34},
+                      1: {"patch_size": 224, "spacing": 56},
                       2: {'patch_size':224, 'spacing':56, 'comment':'larger margin'},
                      3: {'patch_size':224, 'spacing':32, 'comment':'smaller spacing'},
                      4: {'patch_size':224, 'spacing':128, 'comment':'smaller spacing'},
