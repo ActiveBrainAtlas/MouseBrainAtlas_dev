e51a72a8b64b900dc9d8be479547cac6c661575e
Newberry <saintdane75@gmail.com>
2019-08-21 15:57:33 -0700

    Update run_docker.sh

--- doc/docker/run_docker.sh
+++ doc/docker/run_docker.sh
@@ -1,9 +1,9 @@
-```#!/bin/bash
+#!/bin/bash
 
 # Insist that the root filepath be passed in
 if [ "$1" == "" ]; then
 	echo "Argument ROOT_FP not passed in."
-	echo "source thisScript.sh \$ROOT_FP"
+	echo "source run_docker.sh \$ROOT_FP"
 	return 0
 fi
 
@@ -11,4 +11,4 @@ xhost +local:root
 
 docker run -it --env="DISPLAY" --env="QT_X11_NO_MITSHM=1" --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" -v "$1":"/mnt/data" -v "/":"/mnt/computer_root" anewberry/atlas_demo:atlas_v0.0.3
 
-xhost -local:root```
+xhost -local:root
