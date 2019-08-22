4477a8f9d2fb0b9ae8a8d40fd91aa1dda8d91b3f
Newberry <saintdane75@gmail.com>
2019-08-22 14:54:33 -0700

    Create setup_kakadu_docker.sh

---
+++ setup/setup_kakadu_docker.sh
@@ -0,0 +1,9 @@
+curl \
+https://kakadusoftware.com/wp-content/uploads/2014/06/\
+KDU7A2_Demo_Apps_for_Ubuntu-x86-64_170827.zip \
+--output \
+/app/KDU7A2_Demo_Apps_for_Ubuntu-x86-64_170827.zip
+
+unzip /app/KDU7A2_Demo_Apps_for_Ubuntu-x86-64_170827.zip
+
+rm /app/KDU7A2_Demo_Apps_for_Ubuntu-x86-64_170827.zip
