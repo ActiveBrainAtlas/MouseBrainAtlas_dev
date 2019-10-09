7dcc1a0ee87874c515238a01bcac7231c9813fc9
Newberry <saintdane75@gmail.com>
2019-10-08 18:39:46 -0700

    Update setup_bftools_docker.sh

--- setup/setup_bftools_docker.sh
+++ setup/setup_bftools_docker.sh
@@ -4,6 +4,6 @@ curl \
 -o /app/bftools.zip \
 https://downloads.openmicroscopy.org/bioformats/6.2.1/artifacts/bftools.zip
 
-unzip /app/bftools.zip
+unzip /app/bftools.zip -d /app/
 
 rm /app/bftools.zip
