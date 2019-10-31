455d7ddf9d67ef3b69afac17604b26ff32ff0e09
CodingDonky <saintdane75@gmail.com>
2019-10-31 14:09:29 -0700

    minor updates

--- demo/a_GUI_setup_orientation.py
+++ demo/a_GUI_setup_orientation.py
@@ -39,9 +39,11 @@ stack = args.stack
 #    section_to_fn[i] = img_name
     
 # Assume we have the sorted_filenames file. Load images a different way
-if os.path.exists( DataManager.get_sorted_filenames_filename(stack=stack) ) or \
-os.path.exists( os.path.join( '/mnt/computer_root/'+DataManager.get_sorted_filenames_filename(stack=stack)) ):
-    section_to_fn = metadata_cache['sections_to_filenames'][stack]
+if os.path.exists( DataManager.get_sorted_filenames_filename(stack=stack) ):
+    section_to_fn_sfns = metadata_cache['sections_to_filenames'][stack]
+    section_to_fn = {}
+    for sec in section_to_fn_sfns.keys():
+        section_to_fn[sec] = section_to_fn_sfns[sec]+'_raw.tif'
     fn_list = metadata_cache['filenames_to_sections'][stack].keys()
     fn_list.sort()
     
