c17e31f9671d7dade1cb8d9f170ade3b2e58c5cf
CodingDonky <saintdane75@gmail.com>
2019-11-07 15:14:45 -0800

    minor changes

--- demo/a_GUI_main.py
+++ demo/a_GUI_main.py
@@ -227,7 +227,8 @@ class init_GUI(QWidget):
                     continue
                 # Add a brain to "new_stack" list if it is found and is not a part of "all_stacks"
                 new_stacks.append( brain_name )
-        if not new_stacks==[]:    
+        if not new_stacks==[]:
+            self.cb.addItems( ['a','b'] )
             self.cb.addItems( new_stacks )
         
     def format_grid_buttons(self):
