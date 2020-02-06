6afd8422ffa91054cb7f5730417a83ed6487b6c1
CodingDonky <saintdane75@gmail.com>
2020-02-06 14:19:30 -0800

    testing

--- demo/align_compose.py
+++ demo/align_compose.py
@@ -18,5 +18,5 @@ import sys
 sys.path.append(os.environ['REPO_DIR'] + '/utilities')
 from utilities2015 import execute_command
 
-execute_command('python align_v3.py %s --op %s' % (args.input_spec, args.op))
+#execute_command('python align_v3.py %s --op %s' % (args.input_spec, args.op))
 execute_command('python compose_v3.py %s --op %s' % (args.input_spec, args.op))
